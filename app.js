2a94bfe08a76568de46553df1f11c510a04fbd35
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-11 12:59:29 +0200

    DoneCounter

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
-// set initial count
-// let count = 0;
-
-// // select value and buttons
-// const value = document.querySelector('#value');
-// const btns = document.querySelectorAll('.btn');
-
-// btns.forEach(function (btn) {
-//     btn.addEventListener('click', (e) => {
-//         const styles = e.currentTarget.classList;
-//         if (styles.contains('decrease')) {
-//             count--;
-//         }
-//         else if (styles.contains('increase')) {
-//             count++;
-//         } else {
-//             count = 0;
-//         }
-//         if (count > 0) {
-//             value.style.color = 'green';
-//         }
-//         if (count < 0) {
-//             value.style.color = 'red';
-//         }
-//         if (count === 0) {
-//             value.style.color = 'pink';
-//         }
-//         value.textContent = count;
-//     });
-// });
-
 let count = 0;
 
 const value = document.querySelector('#value');
@@ -38,10 +7,19 @@ btns.forEach(function (btn) {
     btn.addEventListener('click', (e) => {
         const styles = e.currentTarget.classList;
 
-        if (styles.contains('decrease')) {
+        if (styles.contains('increase')) {
             count++;
         } else if (styles.contains('decrease')) {
             count--;
+        } else {
+            count = 0;
+        } if (count > 0) {
+            value.style.color = 'green';
+        } if (count < 0) {
+            value.style.color = 'maroon';
+        } if (count === 0) {
+            value.style.color = 'yellow';
         }
+        value.textContent = count;
     });
 });
